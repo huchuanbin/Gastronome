972fa4e321da5a859f9a4237c2db7119b99dc554
huchuanbin <hcb19940121@vip.qq.com>
2016-03-23 19:09:21 +0800

    主界面和详情页

---
+++ app/src/main/java/com/hcb/gastronome/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.hcb.gastronome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Corbin on 2016/3/23.
+ */
+public class DetailActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/hcb/gastronome/MainActivity.java
+++ app/src/main/java/com/hcb/gastronome/MainActivity.java
@@ -1,9 +1,13 @@
 package com.hcb.gastronome;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -12,20 +16,38 @@ import com.thinkland.sdk.android.DataCallBack;
 import com.thinkland.sdk.android.JuheData;
 import com.thinkland.sdk.android.Parameters;
 
-public class MainActivity extends AppCompatActivity {
-DrawerLayout drawerLayout;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @Bind(R.id.tabLayout)
+    TabLayout tabLayout;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.navigation_view)
+    NavigationView navigationView;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @OnClick(R.id.btn)
+    public void btn() {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView view= (NavigationView) findViewById(R.id.navigation_view);
-view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
-    }
-});
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        initInstances();
 
         /**
          * 请不要添加key参数.
@@ -33,14 +55,14 @@ view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelect
         Parameters params = new Parameters();
         params.add("ip", "www.juhe.cn");
 //        params.add("dtype", "xml");
-        params.add("menu","糖醋排骨");
+        params.add("menu", "糖醋排骨");
 /**
  * 请求的方法 参数: 第一个参数 当前请求的context 第二个参数 接口id 第三二个参数 接口请求的url 第四个参数 接口请求的方式
  * 第五个参数 接口请求的参数,键值对com.thinkland.sdk.android.Parameters类型; 第六个参数
  * 请求的回调方法,com.thinkland.sdk.android.DataCallBack;
  *
  */
-        JuheData.executeWithAPI(this, 46,"http://apis.juhe.cn/cook/query",JuheData.GET, params, new DataCallBack() {
+        JuheData.executeWithAPI(this, 46, "http://apis.juhe.cn/cook/query", JuheData.GET, params, new DataCallBack() {
             /**
              * 请求成功时调用的方法 statusCode为http状态码,responseString    *为请求返回数据.
              */
@@ -57,19 +79,31 @@ view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelect
             @Override
             public void onFinish() {
                 // TODO Auto-generated method stub
-                Toast.makeText(getApplicationContext(), "finish",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "finish", Toast.LENGTH_SHORT).show();
             }
+
             /**
              * 请求失败时调用的方法,statusCode为http状态码,throwable为捕获到的异常
              * statusCode:30002 没有检测到当前网络. 30003 没有进行初始化. 0
              * 未明异常,具体查看Throwable信息. 其他异常请参照http状态码.
              */
             @Override
-            public void onFailure(int statusCode,String responseString, Throwable throwable) {
+            public void onFailure(int statusCode, String responseString, Throwable throwable) {
                 // TODO Auto-generated method stub
 //                tv.append(throwable.getMessage() + "\n");
                 Log.d("MainActivity", throwable.getMessage());
             }
         });
+    }
+
+    private void initInstances() {
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 3"));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }
