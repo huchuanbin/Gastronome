a3e2376504343d65e71bd0e605eee67c53bd9bf1
huchuanbin <543624600@qq.com>
2016-03-31 09:27:21 +0800

    沉浸式状态栏

--- app/src/main/java/com/hcb/gastronome/ui/fragment/MineFragment.java
+++ app/src/main/java/com/hcb/gastronome/ui/fragment/MineFragment.java
@@ -1,27 +1,31 @@
 package com.hcb.gastronome.ui.fragment;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.hcb.gastronome.R;
 import com.hcb.gastronome.ui.base.BaseFragment;
 
+import butterknife.Bind;
 import dagger.Module;
 
 /**
  * Created by huchuanbin on 16/3/30.
  */
 public class MineFragment extends BaseFragment {
+    @Bind(R.id.rl_bar)
+    RelativeLayout rlBar;
     public static MineFragment getInstance() {
         return new MineFragment();
     }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-
+        adaptStatusBar(rlBar);
     }
 
     @Override
     protected int getLayout() {
-        return R.layout.fragment_home;
+        return R.layout.fragment_mine;
     }
 }
